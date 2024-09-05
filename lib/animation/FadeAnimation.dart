b03c4b43e8ee794177599dae4bea60f9992d4ff1
Javid Ibra <mr_javid95@mail.ru>
2024-09-05 10:42:55 +0400

    First commit

---
+++ lib/animation/FadeAnimation.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+// // import 'package: simple_animations/simple_animations.dart';
+
+
+// class FadeAnimation extends StatelessWidget {
+//   final double delay;
+//   final Widget child;
+
+//   const FadeAnimation({super.key, required this.delay, required this.child});
+
+//   @override
+//   Widget build(BuildContext context) {
+
+// final tween = MultiTrackTween(
+//   [
+//     Track["opacity"].add(Duration())
+//   ]
+// )
+
+//     return Container();
+//   }
+// }
